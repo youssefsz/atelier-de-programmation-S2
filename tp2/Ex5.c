72dfb2271bce736145caf404caad30213e83549b
youssefsz <darhoumyi@gmail.com>
2026-02-26 09:59:13 +0100

    tp2 : Ex5 added comments

--- tp2/Ex5.c
+++ tp2/Ex5.c
@@ -1,42 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-   int n , A[50] ;
+int main() {
+  int n, A[50];
 
-   printf("\nsaisir n : ");
-   scanf("%d",&n);
-   for (int i = 0; i<n ; i++){
-        printf("\nsaisir un entier :");
-        scanf("%d",&A[i]);
-        A[i] = A[i] * A[i];
+  printf("\n-------Tableau indicé--------");
 
-   }
-   for (int i = 0; i<n ; i++){
-        printf("\n %d", A[i] );
+  printf("\nsaisir n : ");
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++) {
+    printf("\nsaisir un entier :");
+    scanf("%d", &A[i]);
+    A[i] = A[i] * A[i];
+  }
+  for (int i = 0; i < n; i++) {
+    printf("\n %d", A[i]);
+  }
 
-   }
+  printf("\n---------------Pointeur----------------");
 
-   printf("\n---------------");
+  int *p;
+  p = A;
+  for (p = A; p < A + n; p++) {
+    printf("\nsaisir un entier :");
+    scanf("%d", &*p);
+    *p = *p * *p;
+  }
+  for (p = A; p < A + n; p++) {
+    printf("\n %d", *p);
+  }
 
-   int *p; 
-   p= A;
-   for (p = A ; p<A+n ; p++){
-        printf("\nsaisir un entier :");
-        scanf("%d",&*p);
-        *p = *p * *p;
-
-   }
-   for (p = A ; p<A+n ; p++){
-        printf("\n %d", *p );
-
-   }
-
-   
-
-
-    
-
-
-
-    return 0;
+  return 0;
 }
