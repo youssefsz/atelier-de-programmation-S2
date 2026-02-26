573308a898d3d41e70b53ddc7c5983c68aeb8a2b
youssefsz <darhoumyi@gmail.com>
2026-02-26 10:35:56 +0100

    Tp2: Ex6 fix table type from int to float

--- tp2/Ex6.c
+++ tp2/Ex6.c
@@ -1,51 +1,50 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int saisir_taille() {
-    int n;
-    do {
-        printf("\nsaisir n :");
-        scanf("%d", &n);
-    } while (n <= 0 || n >= 100);
-    return n;
+void saisir_taille(int *n) {
+  do {
+    printf("\nsaisir n :");
+    scanf("%d", n);
+  } while (*n <= 0 || *n >= 100);
 }
 
-void remplir_tableau(int *T, int n, char nom) {
-    int *p;
-    for (p = T; p < T + n; p++) {
-        printf("\nsaisir un entier %c :", nom);
-        scanf("%d", p);
-    }
+void remplir_tableau(float *T, int n, char nom) {
+  float *p;
+  for (p = T; p < T + n; p++) {
+    printf("\nsaisir un entier %c :", nom);
+    scanf("%f", p);
+  }
 }
 
-void calculer_somme(int *U, int *V, int *T, int n) {
-    int *pu = U, *pv = V, *pt = T;
-    while (pt < T + n) {
-        *pt = *pu + *pv;
-        pu++;
-        pv++;
-        pt++;
-    }
+void calculer_somme(float *U, float *V, float *T, int n) {
+  float *pu = U, *pv = V, *pt = T;
+  while (pt < T + n) {
+    *pt = *pu + *pv;
+    pu++;
+    pv++;
+    pt++;
+  }
 }
 
-void afficher_tableau(int *T, int n) {
-    int *p;
-    for (p = T; p < T + n; p++) {
-        printf("\n %d", *p);
-    }
+void afficher_tableau(float *T, int n) {
+  float *p;
+  for (p = T; p < T + n; p++) {
+    printf("\n %f", *p);
+  }
 }
 
 int main() {
-    int n, U[100], V[100], T[100];
-    
-    n = saisir_taille();
-    
-    remplir_tableau(U, n, 'U');
-    printf("\n---------------\n");
-    remplir_tableau(V, n, 'V');
-    
-    calculer_somme(U, V, T, n);
-    
-    afficher_tableau(T, n);
+  int n;
+  float U[100], V[100], T[100];
 
-    return 0;
+  saisir_taille(&n);
+
+  remplir_tableau(U, n, 'U');
+  printf("\n---------------\n");
+  remplir_tableau(V, n, 'V');
+
+  calculer_somme(U, V, T, n);
+
+  afficher_tableau(T, n);
+
+  return 0;
 }
